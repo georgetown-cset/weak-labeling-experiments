0de33b600852f9a21a2d8093dd9e5ef1c00b52a3
collinsnji <developer@collingrimm.me>
2020-07-10 18:41:17 -0400

    changes to data imoport tool

--- weak_labeling/data_import.py
+++ weak_labeling/data_import.py
@@ -1,12 +1,27 @@
 import pandas as pd
 import os
 import csv
+import json
+import re
 from os import path
 import numpy as np
 from google.cloud import bigquery
 
 project_ID = "***REMOVED***"
 client = bigquery.Client(project=project_ID)
+
+
+def build_query(query_regex: str, limit=1000):
+    return f"""
+            select paper.PaperId, paper.PaperTitle, paper.Abstract, RAND() as rand
+                from gcp_cset_mag.PapersWithAbstracts paper
+                left join gcp_cset_mag.PaperFieldsOfStudy paper_fos on paper.PaperId = paper_fos.PaperId
+                left join gcp_cset_mag.FieldsOfStudy fieldOfStudy on paper_fos.FieldOfStudyId = fieldOfStudy.FieldOfStudyId
+                left join `***REMOVED***.cn498_sandbox.mag_lid` lang_id on cast(paper.PaperId as string) = lang_id.id
+            where regexp_contains(NormalizedName, {query_regex})
+            and (DocType != "Dataset") and (DocType != "Patent") and (paper.Abstract is not null) and (paper.Abstract != "") and (lower(lang_id.title_lid) = "english") and (lower(lang_id.abstract_lid) = "english")
+            order by rand desc limit {limit}
+            """
 
 
 def get_training_data(training_data_path: str, from_local_disk: bool, save_to_local: bool = True):
@@ -16,36 +31,12 @@ def get_training_data(training_data_path: str, from_local_disk: bool, save_to_lo
             frac=1, random_state=123).reset_index(drop=True)
         return data
     else:
-        sql_likely_relevant = """
-            select paper.PaperId, paper.PaperTitle, paper.Abstract, RAND() as rand
-                from gcp_cset_mag.PapersWithAbstracts paper
-                left join gcp_cset_mag.PaperFieldsOfStudy paper_fos on paper.PaperId = paper_fos.PaperId
-                left join gcp_cset_mag.FieldsOfStudy fieldOfStudy on paper_fos.FieldOfStudyId = fieldOfStudy.FieldOfStudyId
-                left join `gcp-gu-cset-analysis`.cn498_sandbox.mag_lid lang_id on cast(paper.PaperId as string) = lang_id.id
-            where regexp_contains(NormalizedName, r"(?i)vehicle.*identification$|driving.*automation|connected vehicle|driver.*assistance")
-            and (DocType != "Dataset") and (DocType != "Patent") and (paper.Abstract is not null) and (paper.Abstract != "") and (lower(lang_id.title_lid) = "english") and (lower(lang_id.abstract_lid) = "english")
-            order by rand desc limit 5000
-            """
-        sql_likely_confusable = """
-        select paper.PaperId, paper.PaperTitle, paper.Abstract, RAND() as rand
-            from gcp_cset_mag.PapersWithAbstracts paper
-            left join gcp_cset_mag.PaperFieldsOfStudy paper_fos on paper.PaperId = paper_fos.PaperId
-            left join gcp_cset_mag.FieldsOfStudy fieldOfStudy on paper_fos.FieldOfStudyId = fieldOfStudy.FieldOfStudyId
-            left join `gcp-gu-cset-analysis`.cn498_sandbox.mag_lid lang_id on cast(paper.PaperId as string) = lang_id.id
-        where regexp_contains(fieldOfStudy.NormalizedName, r"(?i)computer vision")
-        and (DocType != "Dataset") and (DocType != "Patent") and (paper.Abstract is not null) and (paper.Abstract != "") and (lower(lang_id.title_lid) = "english") and (lower(lang_id.abstract_lid) = "english")
-        order by rand desc limit 1000
-        """
-        sql_likely_irrelevant = """
-        select paper.PaperId, paper.PaperTitle, paper.Abstract, RAND() as rand
-            from gcp_cset_mag.PapersWithAbstracts paper
-            left join gcp_cset_mag.PaperFieldsOfStudy paper_fos on paper.PaperId = paper_fos.PaperId
-            left join gcp_cset_mag.FieldsOfStudy fieldOfStudy on paper_fos.FieldOfStudyId = fieldOfStudy.FieldOfStudyId
-            left join `gcp-gu-cset-analysis`.cn498_sandbox.mag_lid lang_id on cast(paper.PaperId as string) = lang_id.id
-        where not regexp_contains(NormalizedName, r"(?i)vehicle.*identification$|driving.*automation|connected vehicle|driver.*assistance")
-        and (DocType != "Dataset") and (DocType != "Patent") and (paper.Abstract is not null) and (paper.Abstract != "") and (lower(lang_id.title_lid) = "english") and (lower(lang_id.abstract_lid) = "english")
-        order by rand desc limit 1000
-        """
+        regex_relevant = r"\"(?i)vehicle.*identification$|driving.*automation|connected vehicle|driver.*assistance\""
+        regex_irrelevant = r"\"(?i)vehicle.*identification$|driving.*automation|connected vehicle|driver.*assistance\""
+        regex_confusable = r"\"(?i)computer vision\""
+        sql_likely_relevant = build_query(regex_relevant, 5000)
+        sql_likely_confusable = build_query(regex_confusable, 1000)
+        sql_likely_irrelevant = build_query(regex_irrelevant, 1000)
 
         likely_relevant = pd.read_gbq(
             query=sql_likely_relevant, project_id=project_ID, dialect="standard")
@@ -62,5 +53,25 @@ def get_training_data(training_data_path: str, from_local_disk: bool, save_to_lo
         return df
 
 
+def regex_from_keywords(words: str):
+    word_list = words.split(sep=",")
+    word_list = map(lambda w: w.strip().replace(" ", ".*"), word_list)
+    reg_exp = f"r\"(?i){'|'.join(word_list)}\""
+    return str(reg_exp)
+
+
+def wipo_training_data(keywords_path: str = "../data/keyword_categories_custom.csv", train_size: int = 10):
+    keywords_categories = pd.read_csv(keywords_path, prefix=None)
+    with open(file="../data/train.csv", mode="a+") as training_data_dump:
+        for words in keywords_categories.ImprovedLikelyPhrases:
+            reg_exp = regex_from_keywords(words=words)
+            data_query = build_query(reg_exp, 10)
+            likely_relevant = pd.read_gbq(
+                query=data_query, project_id=project_ID, dialect="standard")
+            likely_relevant.to_csv(training_data_dump, mode="a",  encoding="utf-8", header=False, index=False,
+                                   quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+
+
 if __name__ == "__main__":
-    get_training_data(training_data_path="../data/training_data.csv", from_local_disk=True, save_to_local=True)
+    # get_training_data(training_data_path="../data/training_data.csv", from_local_disk=True, save_to_local=True)
+    wipo_training_data()
