55731300e6880f60d81da6b2fbeaa08c2cfca976
Jennifer Melot <jtmelot@gmail.com>
2020-07-21 12:29:39 -0400

    mods to output total and median statistics

--- weak_labeling/class_balance_estimator.py
+++ weak_labeling/class_balance_estimator.py
@@ -1,42 +1,61 @@
-from lf_generator import data_import, create_wordlist_set
-from tqdm import tqdm
-import re
-import pandas as pd
-from typing import List
+import csv
 import json
+import re
+import statistics
+
+from google.cloud import bigquery
 
 
-def estimate_class_balance(word_list_set: set, kw_dict: List[dict], train_data: pd.DataFrame):
-    # This is but a proof of concept. I'm sure there are better ways of doing this
-    train_data = train_data[1000:]
-    paper_titles = " ".join(train_data.papertitle)
-    paper_abstracts = " ".join(train_data.abstract)
-    # Is there a better way of doing this? Not exactly sure this could be done in BQ
-    data = f"{paper_titles} {paper_abstracts}"
-    corpus_size = train_data.shape[0]
-
-    match_values = {}
-    for i in tqdm(word_list_set):
-        # TODO: use the proper regular expression. For now we just use the keywords as is
-        matches = len(re.findall(fr"(?i){i}", data))
-        match_values[i] = float(matches/corpus_size)
-
-    wipo_class_balance_avg = {}
-    for i, keyword in enumerate(kw_dict):
-        wordlist = keyword["ImprovedLikelyPhrases"].split(sep=", ")
-        average_freq = (sum([match_values.get(key) for key in wordlist]))/len(wordlist)
-        wipo_class_balance_avg[keyword["Id"]] = average_freq
-
-    with open('result.json', 'w') as out:
-        json.dump(wipo_class_balance_avg, out)
+def format_keyword(kw: str):
+    words = kw.strip().split()
+    return r"(?i)"+r"\S*\s+".join(words)
 
 
-def main():
-    df_train, df_keywords, keywords_dict = data_import()
-    likely_phrases = df_keywords.ImprovedLikelyPhrases
-    word_list_set = create_wordlist_set(likely_phrases)
-    estimate_class_balance(word_list_set, keywords_dict, df_train)
+def get_cat_id_to_keywords(kw_path: str):
+    cat_id_to_keywords = {}
+    for line in csv.DictReader(open(kw_path)):
+        cat_id_to_keywords[line["Id"]] = [format_keyword(k) for k in line["ImprovedLikelyPhrases"].split(",")]
+    return cat_id_to_keywords
+
+
+def estimate_class_balance():
+    client = bigquery.Client(project="***REMOVED***")
+    cat_to_keywords = get_cat_id_to_keywords("../data/keyword_categories_custom.csv")
+    print(cat_to_keywords)
+    cat_counts, kw_counts = {}, {}
+    for cat, kws in cat_to_keywords.items():
+        cat_counts[cat] = 0
+        for kw in kws:
+            kw_counts[kw] = 0
+    corpus_size = 0
+
+    for record in client.list_rows("cn498_sandbox.mag_subset_for_snorkel_eval"):
+        if corpus_size % 1000 == 0:
+            print("on "+str(corpus_size))
+        corpus_size += 1
+        text = record["papertitle"]+" "+record["abstract"]
+        for cat in cat_to_keywords:
+            has_match = False
+            for kw in cat_to_keywords[cat]:
+                if re.search(kw, text):
+                    kw_counts[kw] += 1
+                    has_match = True
+            if has_match:
+                cat_counts[cat] += 1
+
+    # write out the frequency with which *any* of a category's kws match
+    with open('cat_total_freq.json', 'w') as out:
+        cat_to_total_freq = {cat: hits/corpus_size for cat, hits in cat_counts.items()}
+        json.dump(cat_to_total_freq, out)
+
+    # write out the median frequency of match over all of a category's kws
+    with open('cat_median_freq.json', 'w') as out:
+        cat_to_median_freq = {}
+        for cat in cat_to_keywords:
+            rel_kw_freqs = [kw_counts[kw]/corpus_size for kw in cat_to_keywords[cat]]
+            cat_to_median_freq[cat] = statistics.median(rel_kw_freqs)
+        json.dump(cat_to_median_freq, out)
 
 
 if __name__ == "__main__":
-    main()
+    estimate_class_balance()
