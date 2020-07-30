a3daa2dd9c2685162f38b6493a35d8d6b982be8c
Jennifer Melot <jtmelot@gmail.com>
2020-07-30 13:00:13 +0000

    logging avg as well as median

--- weak_labeling/class_balance_estimator.py
+++ weak_labeling/class_balance_estimator.py
@@ -31,13 +31,15 @@ def get_frequencies(fi: str, cat_to_keywords: dict):
         record = json.loads(line)
         corpus_size += 1
         text = record["papertitle"]+" "+record["abstract"]
-        for cat in cat_to_keywords:
-            has_match = False
-            for kw in cat_to_keywords[cat]:
-                if re.search(kw, text):
-                    kw_counts[kw] += 1
-                    has_match = True
-            if has_match:
+        cat_to_has_match = {cat: False for cat in cat_counts}
+        for kw in kw_counts:
+            if re.search(kw, text):
+                kw_counts[kw] += 1
+                for cat in cat_to_keywords:
+                    if kw in cat_to_keywords[cat]:
+                        cat_to_has_match[cat] = True
+        for cat in cat_to_has_match:
+            if cat_to_has_match[cat]:
                 cat_counts[cat] += 1
     return {
         "corpus_size": corpus_size,
@@ -56,21 +58,31 @@ def estimate_class_balance(input_dir: str):
     # take the first element to get the key sets
     one_elt = freq_info[0]
     corpus_size = sum([e["corpus_size"] for e in freq_info])
+    print(f"corpus size was {corpus_size}")
     kw_counts = {kw: sum([e["kw_counts"][kw] for e in freq_info]) for kw in one_elt["kw_counts"]}
     cat_counts = {cat: sum([e["cat_counts"][cat] for e in freq_info]) for cat in one_elt["cat_counts"]}
 
-    # write out the frequency with which *any* of a category's kws match
-    with open('cat_total_freq.json', 'w') as out:
+    # write out the frequency with which *any* of a category"s kws match
+    with open("cat_total_freq.json", "w") as out:
         cat_to_total_freq = {cat: hits/corpus_size for cat, hits in cat_counts.items()}
         json.dump(cat_to_total_freq, out)
 
-    # write out the median frequency of match over all of a category's kws
-    with open('cat_median_freq.json', 'w') as out:
+    # write out the median frequency of match over all of a category"s kws
+    with open("cat_median_freq.json", "w") as out:
         cat_to_median_freq = {}
         for cat in cat_to_keywords:
             rel_kw_freqs = [kw_counts[kw]/corpus_size for kw in cat_to_keywords[cat]]
             cat_to_median_freq[cat] = statistics.median(rel_kw_freqs)
         json.dump(cat_to_median_freq, out)
+
+    # write out the average frequency of match over all of a category"s kws
+    with open("cat_average_freq.json", "w") as out:
+        cat_to_average_freq = {}
+        for cat in cat_to_keywords:
+            rel_kw_freqs = [kw_counts[kw]/corpus_size for kw in cat_to_keywords[cat]]
+            cat_to_average_freq[cat] = sum(rel_kw_freqs)/len(rel_kw_freqs)
+        json.dump(cat_to_average_freq, out)
+
 
 
 if __name__ == "__main__":
